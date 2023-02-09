0342d0daddb495c7130d595bb589fb929fd830a4
Guilherme Lucena <guilherme.lucena17@gmail.com>
2023-02-09 19:04:27 -0300

    requisito 13

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,13 +1,38 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
 class Feedback extends Component {
   render() {
+    const { score } = this.props;
+
+    const minScore = 3;
+
+    const feedback = {
+      lessThanThree: 'Could be better...',
+      threeOrMore: 'Well Done!',
+    };
+
     return (
       <div>
-        <p> </p>
+        <header>
+          <img src="" alt="user" data-testid="header-player-name" />
+          <p data-testid="header-player-name">player name</p>
+          <p data-testid="header-score">player score</p>
+        </header>
+        <main>
+          <p data-testid="feedback">
+            { score < minScore
+              ? feedback.lessThanThree
+              : feedback.threeOrMore }
+          </p>
+        </main>
       </div>
     );
   }
 }
+
+Feedback.propTypes = {
+  score: PropTypes.number.isRequired,
+};
 
 export default Feedback;
