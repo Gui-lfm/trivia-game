499ca681fee12d9fd60f64f8eab2d7f592f7bd47
Felipe Tavares <xfelipetavares@gmail.com>
2023-02-11 19:17:13 -0300

    bug fixed

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,8 @@
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import styles from './styles.module.css';
-// import defaultAvatar from '../../assets/default-avatar.png';
 import starScore from '../../assets/star-score.svg';
-import gearSettings from '../../assets/gear-settings.svg';
-// import * as AllActions from '../../redux/actions';
+import SettingsButton from '../SettingsButton';
 
 function Header({ score, name }) {
   const img = 'https://www.gravatar.com/avatar/c19ad9dbaf91c5533605fbf985177ccc';
@@ -20,17 +18,17 @@ function Header({ score, name }) {
           />
           <p data-testid="header-player-name">{name}</p>
         </div>
-        <div className={ styles.userPoints }>
+        <div className={ styles.userScore }>
           <img src={ starScore } alt="A star representing your score" />
+          <p>
+            Pontos:
+            {' '}
+          </p>
           <p data-testid="header-score">
-            {/* Pontos:
-            {' '} */}
             {score}
           </p>
         </div>
-        <button>
-          <img src={ gearSettings } alt="A gear representing settings" />
-        </button>
+        <SettingsButton />
       </div>
     </header>
   );
