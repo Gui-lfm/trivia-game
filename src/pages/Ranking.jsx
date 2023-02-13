ab8aed3310ae4c5d3d5d628eeb9ff4dbd6bd97b7
Filipe Bueno <filipe_bueno85@live.com>
2023-02-13 09:01:29 -0300

    Requisito 19

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Ranking extends Component {
   render() {
     return (
       <div>
         <h2 data-testid="ranking-title">Ranking</h2>
+        <Link to="/">
+          <button
+            data-testid="btn-go-home"
+          >
+            Início
+          </button>
+        </Link>
       </div>
     );
   }
